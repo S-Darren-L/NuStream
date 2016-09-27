166fce3f8f33b8f3197f7ef96f5a2b7f25edad2e
Darren--Liu <s.darren.liu@gmail.com>
2016-09-27 01:13:19 -0400

    create supplier

---
+++ create-supplier-template.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+Template Name: Create Supplier Layout
+*/
+
+get_header();
+?>
+<?php
+	if(isset($_POST['create_supplier']))
+	{
+		$createSupplierArray = array (
+			"supplierID" => GUID(),
+			"supplierName" => $_POST['supplierName'],
+			"HSTNumber" => $_POST['HSTNumber'],
+			"firstContactName" => $_POST['firstContactName'],
+			"firstContactNumber" => $_POST['firstContactNumber'],
+			"secondContactName" => $_POST['secondContactName'],
+			"secondContactNumber" => $_POST['secondContactNumber'],
+			"priceUnit" => $_POST['priceUnit'],
+			"pricePerUnit" => $_POST['pricePerUnit'],
+			"paymentTerm" => $_POST['paymentTerm'],
+			"supportLocation" => $_POST['supportLocation']);
+
+		create_supplier($createSupplierArray);
+	}
+?>
+<div style="overflow-x:auto;">
+	<form method="post">
+		<table class="supplier-temp-table">
+			<tr>
+				<td class="primary-title" colspan="3"><a>Supplier Name</a></td>
+				<td class="name" colspan="3"><input class="input" type="text" name="supplierName"></td>
+				<td class="primary-title" colspan="3"><a>HST Number</a></td>
+				<td class="number" colspan="6"><input class="input" type="text" name="HSTNumber"></td>
+			</tr>
+			<tr>
+				<td class="primary-title" colspan="3"><a>Contact Name 1</a></td>
+				<td class="name" colspan="3"><input class="input" type="text" name="firstContactName"></td>
+				<td class="primary-title" colspan="3"><a>Contact Number 1</a></td>
+				<td class="number" colspan="6"><input class="input" type="text" name="firstContactNumber"></td>
+			</tr>
+			<tr>
+				<td class="primary-title" colspan="3"><a>Contact Name 2</a></td>
+				<td class="name" colspan="3"><input class="input" type="text" name="secondContactName"></td>
+				<td class="primary-title" colspan="3"><a>Contact Number 2</a></td>
+				<td class="number" colspan="6"><input class="input" type="text" name="secondContactNumber"></td>
+			</tr>
+			<tr>
+				<td class="primary-title" colspan="3"><a>Price Unit</a></td>
+				<td class="small-sub-title" colspan="2"><a>by size</a></td>
+				<td class="radio"><input type="radio" name="priceUnit"></td>
+				<td class="small-sub-title" colspan="2"><a>by hour</a></td>
+				<td class="radio"><input type="radio" name="priceUnit"></td>
+				<td class="small-sub-title" colspan="2"><a>by house type</a></td>
+				<td class="radio"><input type="radio" name="priceUnit"></td>
+				<td class="small-sub-title" colspan="2"><a>by case</a></td>
+				<td class="radio"><input type="radio" name="priceUnit"></td>
+			</tr>
+			<tr>
+				<td class="primary-title" colspan="3"><a>Price Per Unit</a></td>
+				<td class="" colspan="12"><input class="input" type="text" name="pricePerUnit"></td>
+			</tr>
+			<tr>
+				<td class="primary-title" colspan="3"><a>Payment Term</a></td>
+				<td class="large-sub-title" colspan="3"><a>monthly</a></td>
+				<td class="radio"><input type="radio" name="paymentTerm"></td>
+				<td class="large-sub-title" colspan="3"><a>semi-monthly</a></td>
+				<td class="radio"><input type="radio" name="paymentTerm"></td>
+				<td class="large-sub-title" colspan="3"><a>other</a></td>
+				<td class="radio"><input type="radio" name="paymentTerm"></td>
+			</tr>
+			<tr>
+				<td class="primary-title" colspan="3"><a>Support Location</a></td>
+				<td class="" colspan="12"><input class="input" type="text" name="supportLocation"></td>
+			</tr>
+			<tr>
+				<td class="primary-title" colspan="3"><a>Sample Photos</a></td>
+				<td class="" colspan="12"></td>
+			</tr>
+		</table>
+		<input type="submit" value="click" name="create_supplier">
+	</form>
+</div>
+<?php
+get_footer();
+
+?>
--- functions.php
+++ functions.php
@@ -1,70 +1,33 @@
 <?php
-	function demo001_resources(){
-		wp_enqueue_style('style', get_stylesheet_uri());
-	}
-	
-	add_action('wp_enqueue_scripts', 'demo001_resources');
+    // Get Project Style
+	add_action('wp_enqueue_scripts', 'nustream_resources');
 	
 	// Navigation Menus
 	register_nav_menus(array(
 		'primary' => __( 'Primary Menu'),
 		'footer' => __( 'Footer Menu'),
 	));
-	
-	
-	// Get top ancestor
-	function get_top_ancestor_id() {
-		global $post;
-		if($post->post_parent) {
-			$ancestors = array_reverse(get_post_ancestors($post->ID));
-			return $ancestors[0];
-		}
-		
-		return $post->ID;
-	}
-	
-	//Does page have children?
-	function has_children() {
-		global $post;
-		$pages = get_pages('child_of=' . $post->Id);
-		return count($pages);
-	}
+
+    // Set Style File
+    function nustream_resources(){
+        wp_enqueue_style('style', get_stylesheet_uri());
+    }
+
+    // Generate Guid
+    function GUID()
+    {
+        if (function_exists('com_create_guid') === true)
+        {
+            return trim(com_create_guid(), '{}');
+        }
+
+        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+    }
 	
 	//Create supplier
-
-        echo htmlspecialchars($_POST['name']);
-	/* function create_supplier() {
-		echo '<script type="text/javascript">alert("It works.");</script>';
-		$supplierName = 
-		$HSTNumber = 
-		$firstContactName = 
-		$firstContactNumber = 
-		$secondContactName = 
-		$secondContactNumber = 
-		$priceUnit = 
-		$pricePerUnit =
-		$paymentTerm = "";
-
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$supplierName = test_input($_POST["supplierName"]);
-			$HSTNumber = test_input($_POST["HSTNumber"]);
-			$firstContactName = test_input($_POST["firstContactName"]);
-			$firstContactNumber = test_input($_POST["firstContactNumber"]);
-			$secondContactName = test_input($_POST["secondContactName"]);
-			$secondContactNumber = test_input($_POST["secondContactNumber"]);
-			$priceUnit = test_input($_POST["priceUnit"]);
-			$pricePerUnit = test_input($_POST["pricePerUnit"]);
-			$paymentTerm = test_input($_POST["paymentTerm"]);
-		}
-		//Test
-		//TODO: call repository to insert data into database
-	}
-	
-	function test_input($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	} */
+    function create_supplier($createSupplierArray) {
+        require_once(__DIR__ . '/include/repository/supplier-repository.php');
+        create_supplier_repository($createSupplierArray);
+    }
 
 ?>
