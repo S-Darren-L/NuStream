d6c7654a93bb831f061cabad396f1b4266cf742f
S-Darren-Liu <gulang15@gmail.com>
2016-10-25 04:33:10 -0400

    accountant files

--- template-accountant-files-management.php
+++ template-accountant-files-management.php
@@ -9,9 +9,40 @@
 
 
     // Init
-    $serviceStatus = '';
-    $allServiceArray = get_all_cases();
+    $serviceStatus = $_GET['FType'];
+//    $allServiceArray = get_all_cases();
     $PageURL = get_home_url() . '/accountant-files-management';
+    $subMenuURL = get_home_url() . "/accountant-files-management/?FType=";
+
+    if(empty($serviceStatus)){
+        $serviceStatus = 'Approved';
+    }
+    // Init
+    $allServices = array();
+    if($serviceStatus !== 'Final'){
+        $allServices = get_all_services($serviceStatus);
+    }else{
+        $allServiceArray = get_all_cases();
+    }
+
+    // Get All Services By Status
+    function get_all_services($serviceStatus){
+        $allServicesResult = get_all_services_with_file_by_status($serviceStatus);
+        while($service = mysqli_fetch_array($allServicesResult))
+        {
+            $caseResult = mysqli_fetch_array(get_case_by_service_type_and_id($service['SupplierType'], $service['ServiceID']));
+            $service['MLS'] = $caseResult['MLS'];
+            $accountResult = mysqli_fetch_array(get_agent_account($caseResult['StaffID']));
+            $service['MemberName'] = $accountResult['FirstName'] . $accountResult['LastName'];
+            $teamResult = mysqli_fetch_array(get_team_by_team_id($accountResult['TeamID']));
+            $service['TeamLeaderName'] = $teamResult['TeamLeaderName'];
+            if($serviceStatus !== 'Report'){
+                $service['File'] = mysqli_fetch_array(download_file_by_path($service['InvoicePath']))['FileName'];
+            }
+            $allServices[] = $service;
+        }
+        return $allServices;
+    }
 
     // Get All Closed Cases
     function get_all_cases(){
@@ -28,9 +59,13 @@
         return $allServiceArray;
     }
 
-    // Download FIle
     if(isset($_GET['File'])){
-        $MLS = $_GET['File'];
+        download_file($_GET['File']);
+    }
+
+    // Download FIle
+    if(isset($_GET['Files'])){
+        $MLS = $_GET['Files'];
         $uploadPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . "Upload" . DIRECTORY_SEPARATOR . "case" . DIRECTORY_SEPARATOR . $MLS . DIRECTORY_SEPARATOR . "finalReport";
         try{
             create_zip($uploadPath, $MLS);
@@ -51,43 +86,101 @@
 <body>
 <div id="container">
     <?php
-        include_once(__DIR__ . '/navigation.php');
+    include_once(__DIR__ . '/navigation.php');
     ?>
     <div id="main">
         <div class="formPart">
-            <div class="ICATitle">
-		<p class="ICATitleSize"><strong>FILES</strong><a class="FACSelect"href="#"><strong>DOWNLOAD SELECTED</strong></a></p>
-	    </div>
-            <table class="FACTable" >
-                <thead">
-                <tr>
-                    <th class="FACTableHeader FACTableMargin">MLS NUMBER</th>
-                    <th class="FACTableHeader">MEMBER NAME</th>
-                    <th class="FACTableHeader">TEAM LEAD</th>
-                    <th class="FACTableHeader">UPLOAD DATE</th>
-                    <th class="FACTableHeader FACTableHeaderLarge">PRICE BEFORE TAX</th>
-                    <th class="FACTableHeader FACTableHeaderSmall">INVOICE</th>
-                    <th class="FACTableHeader FACTableHeaderSmall">SELECT</th></tr>
-                </thead>
-                <tbody>
-                <?php
-                    for($i = 0; $i < count($allServiceArray); $i++) {
-                        echo '<tr ng-repeat="info in data.infoAdmin|orderBy:orderByField:reverseSort">';
-                        echo '<td class="FACTableMargin">', $allServiceArray[$i]['MLS'], '</td>';
-                        echo '<td>', $allServiceArray[$i]['MemberName'], '</td>';
-                        echo '<td>', $allServiceArray[$i]['TeamLeaderName'], '</td>';
-                        echo '<td>', $allServiceArray[$i]['StartDate'], '</td>';
-                        echo '<td>$&nbsp;', $allServiceArray[$i]['FinalPrice'], '&nbsp;CAD</td>';
-                        echo '<td class="FACDownload">', '<a href="' . $PageURL . '/?File=' . $allServiceArray[$i]['MLS'] . '">DOWNLOAD</a>', '</td>';
-                        echo '<td>', '<input class="FACCheckBox" name="select_for_download" checked="checked" type="checkbox">', '</td>';
-                    }
-                ?>
-                </tbody>
-            </table>
-            <div class="FACPageNum"><a href="#">BACK</a>&nbsp;&nbsp;&nbsp;<a href="#">NEXT</a></div>
+            <div class="FATitle"><p class="titleSize"><strong>FILES MANAGEMENT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <?php
+                        echo '<a class="FASubTitle" href="' . $subMenuURL . "Approved" . '">&nbsp;&nbsp;Approved Files&nbsp;&nbsp;</a>|<a class="FASubTitle" href="' . $subMenuURL . "Final" . '">&nbsp;&nbsp;Final Reports&nbsp;&nbsp;</a>';
+                        ?></strong></p>
+            </div>
+            <section ng-app="app" ng-controller="MainCtrl">
+                <table  class="FATable">
+                    <thead>
+                    <tr>
+                        <th class="FATableHead">
+                            <a href="#" ng-click="orderByField='MLSNUMBER'; reverseSort = !reverseSort">MLS NUMBER <!--<span ng-show="orderByField == 'MLSNUMBER'"><span ng-show="!reverseSort"><div class="arrow-up"></div></span><span ng-show="reverseSort"><div class="arrow-down"></div></span></span>--></a>
+                        </th>
+                        <th class="FATableHead">
+                            <a href="#" ng-click="orderByField='MEMBERNAME'; reverseSort = !reverseSort">MEMBER NAME <!--<span ng-show="orderByField == 'MEMBERNAME'"><span ng-show="!reverseSort"><div class="arrow-up"></div></span><span ng-show="reverseSort"><div class="arrow-down"></div></span></span>-->
+                            </a>
+                        </th>
+                        <th class="FATableHead">
+                            <a href="#" ng-click="orderByField='TEAMLEAD'; reverseSort = !reverseSort">TEAM LEAD <!--<span ng-show="orderByField == 'TEAMLEAD'"><span ng-show="!reverseSort"><div class="arrow-up"></div></span><span ng-show="reverseSort"><div class="arrow-down"></div></span></span>-->
+                            </a>
+                        </th>
+                        <th class="FATableHead">
+                            <a href="#" ng-click="orderByField='UPLOADDATE'; reverseSort = !reverseSort">UPLOAD DATE <!--<span ng-show="orderByField == 'UPLOADDATE'"><span ng-show="!reverseSort"><div class="arrow-up"></div></span><span ng-show="reverseSort"><div class="arrow-down"></div></span></span>-->
+                            </a>
+                        </th>
+                        <?php
+                        if($serviceStatus !== 'Final'){
+                            echo '
+                        <th class="FATableHead">
+                            <a href="#" ng-click="orderByField=\'SERVICETYPE\'; reverseSort = !reverseSort">SERVICE TYPE <!--<span ng-show="orderByField == \'SERVICETYPE\'"><span ng-show="!reverseSort"><div class="arrow-up"></div></span><span ng-show="reverseSort"><div class="arrow-down"></div></span></span>-->
+                            </a>
+                        </th>';
+                        }
+                        ?>
+                        <?php
+                        if($serviceStatus !== 'Final'){
+                            echo '<th class="FATableHead FATableHeadLarge">
+                            <a href="#" ng-click="orderByField=\'PRICEBEFFORETAX\'; reverseSort = !reverseSort">PRICE BEFFORE TAX <!--<span ng-show="orderByField == \'PRICEBEFFORETAX\'"><span ng-show="!reverseSort"><div class="arrow-up"></div></span><span ng-show="reverseSort"><div class="arrow-down"></div></span></span>-->
+                            </a>
+                        </th>';
+                        }
+                        ?>
+
+
+                        <th  class="FATableHead FATableHeadSmall">
+                            <a href="#">INVOICE</a>
+                        </th><?php
+                        if($serviceStatus !== 'Final'){
+                            echo '
+                        <th  class="FATableHead FATableHeadSmall">
+                            <a href="#">STATUS</a>
+                        </th>';
+                        }
+                        ?>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <form method="post">
+                        <?php
+                        if($serviceStatus !== 'Final') {
+                            for($i = 0; $i < count($allServices); $i++) {
+                                echo '<tr ng-repeat="info in data.infoAdmin|orderBy:orderByField:reverseSort">';
+                                echo '<td>', '<a href="' . $homeURL . '/admin-case-details/?CID=' . $allServices[$i]['MLS'] . '" />', $allServices[$i]['MLS'], '</td>';
+                                echo '<td>', $allServices[$i]['MemberName'], '</td>';
+                                echo '<td>', $allServices[$i]['TeamLeaderName'], '</td>';
+                                echo '<td>', $allServices[$i]['StartDate'], '</td>';
+                                echo '<td>', $allServices[$i]['SupplierType'], '</td>';
+                                echo '<td>', $allServices[$i]['RealCost'], '</td>';
+                                echo '<td>', '<a href="' . $subMenuURL . $serviceStatus . '&File=' . $allServices[$i]["File"] . '">DOWNLOAD</a>', '</td>';
+                                echo '<td>', $allServices[$i]['InvoiceStatus'], '</td>';
+                            }
+                        }else{
+                            for($i = 0; $i < count($allServiceArray); $i++) {
+                                echo '<tr ng-repeat="info in data.infoAdmin|orderBy:orderByField:reverseSort">';
+                                echo '<td>', $allServiceArray[$i]['MLS'], '</td>';
+                                echo '<td>', $allServiceArray[$i]['MemberName'], '</td>';
+                                echo '<td>', $allServiceArray[$i]['TeamLeaderName'], '</td>';
+                                echo '<td>', $allServiceArray[$i]['StartDate'], '</td>';
+                                echo '<td>', '<a href="' . $subMenuURL . 'Final' . '&Files=' . $allServiceArray[$i]['MLS'] . '">DOWNLOAD</a>', '</td>';
+                            }
+                        }
+                        ?>
+                    </form>
+                    </tbody>
+                </table>
+            </section>
+            <div class="FAPageNum"><a class="FAPageLetter" href="#"><span >BACK</span></a>&nbsp;&nbsp;&nbsp;<a class="FAPageLetter" href="#"><span>NEXT</span></a></div>
         </div>
     </div>
 </div>
+</div>
+<script src="http://cdn.static.runoob.com/libs/angular.js/1.4.6/angular.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
